c5d8345e1d3c8eb990a908750841fc6d6ae364a7
Paul Aglipay <paglipay@gmail.com>
2020-04-22 15:55:30 -0700

    Updated Bootstrap Theme

--- assests/js/script.js
+++ assests/js/script.js
@@ -103,11 +103,13 @@ function renderScore() {
         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
 
         // Add some text to the new cells:
         cell1.innerHTML = (s + 1);
 
-        cell2.innerHTML = userScore["name"] + ":" + userScore["score"];
+        cell2.innerHTML = userScore["name"];
+        cell3.innerHTML = userScore["score"];
 
         //li.appendChild(pElem);
         //ul.appendChild(li);
